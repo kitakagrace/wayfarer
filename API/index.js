9ea5d02dc95ac7ecc4d0bd8661460043fc2217e1
grace <kitakagrace@gmail.com>
2019-07-25 13:17:18 +0300

    ft(story): Add User sign up endpoint [Starts #167508869]

--- API/index.js
+++ API/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const users = require('./db/userDb');
 const PORT = 3000;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.post('/api/v1/signup', (req,res)=>{
+    const userSchema = {
+        email: req.body.email,
+        password:req.body.password,
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+              
+    }
+    jwt.sign({userSchema},'secretkey',(error,token)=>{
+        
+            res.json({
+            status: "success",
+            data:{
+            token:token,
+            email: userSchema.email,
+            first_name: userSchema.first_name,
+            last_name: userSchema.last_name
+            }  
+       })
+       
+    })
+})
 
 
 
