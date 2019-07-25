3fe1d7c09f371bb00f03665f3dc8414d00e509ff
grace <kitakagrace@gmail.com>
2019-07-25 14:29:43 +0300

    ft(story): Add Admin can create trips endpoint [Starts #167509766]

--- API/index.js
+++ API/index.js
@@ -2,11 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const users = require('./db/userDb');
+const db_trips = require('./db/db_trips');
+const db_bookings =require('./db/db_bookings');
 const PORT = 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const checkToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+
+        req.token = token;
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403)
+    }
+}
 
 app.post('/api/v1/signup', (req,res)=>{
     const userSchema = {
@@ -74,6 +91,61 @@ app.post('/api/v1/signin', (req,res)=>{
     })
 })
 
+app.post('/api/v1/trips',checkToken, (req, res) => {
+  
+    if(!req.body.seating_capacity) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'Seating capacity is required'
+      });
+    } else if(!req.body.bus_licence_number) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'Bus licence Number is required'
+      });
+    }else if(!req.body.origin){
+      return res.status(400).send({
+        success: 'false',
+        message: 'Origin is required'
+      });
+    }else if(!req.body.destination){
+      return res.status(400).send({
+        success: 'false',
+        message: 'Destination is required'
+      });
+    }else if(!req.body.trip_date){
+      return res.status(400).send({
+        success: 'false',
+        message: 'Trip date is required'
+      });
+    }else if(!req.body.fare){
+      return res.status(400).send({
+        success: 'false',
+        message: 'fare is required'
+      });
+    }else if(!req.body.status){
+      return res.status(400).send({
+        success: 'false',
+        message: 'status is required'
+      });
+    }
+   const trip = {
+      id: db_trips.length + 1,
+      seating_capacity:req.body.seating_capacity,
+      bus_licence_number: req.body.bus_licence_number,
+      origin: req.body.origin,
+      destination: req.body.destination,
+      trip_date: req.body.trip_date,
+      fare: req.body.fare,
+      status: req.body.status
+   }
+   db_trips.push(trip);
+   return res.status(201).send({
+     success: 'true',
+     message: 'trip added successfully',
+     trip
+   })
+  });
 
 app.listen(PORT, (req,res) =>{
     console.log(`API running at port ${PORT}`)
