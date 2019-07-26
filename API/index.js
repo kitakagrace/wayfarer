54f56b2b36bc4290deb87bfc3b665a76e3f58c8a
grace <kitakagrace@gmail.com>
2019-07-26 12:48:09 +0300

    ft(story): Add admin view bookings [Starts #167533489]

--- API/index.js
+++ API/index.js
@@ -228,9 +228,20 @@ app.post('/api/v1/trips',checkToken, (req, res) => {
    message: 'Booking is successfull',
    booking
  })
- 
+
 });
 
+app.get('/api/v1/bookings', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    data: db_bookings
+  })
+
+  return res.status(404).send({
+    status: 'error',
+    error: 'Bookings not found',
+  });
+});
 app.listen(PORT, (req,res) =>{
     console.log(`API running at port ${PORT}`)
 })
